386645ce7ac52d2b36c4afede6ba0fe25b38f7f2
RigvedIyer123 <86088571+RigvedIyer123@users.noreply.github.com>
2021-12-10 10:53:06 +0800

    Add files via upload

---
+++ age.py
@@ -0,0 +1,9 @@
+print ("number guessing game")
+print ("enter your guess:")
+print("                                                                 ")
+number = int(input("guess a number between 1-9"))
+if (number>4):
+    print("your guess was too high:guess a number lower than" )
+else:
+    print ("your guess was too low: guess a number higher than" )
+
